b915845e862abeadc3e279ac728a9fb0bd27e7c9
Faizan Azim <faizanazim11@gmail.com>
2022-06-07 06:57:52 +0530

    Markers Added with Latitude and Longitude

--- prider/src/app/app-console/app-console.component.ts
+++ prider/src/app/app-console/app-console.component.ts
@@ -4,6 +4,8 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit, ViewChild } from '@angular/core';
 declare const google: any;
 
+let arr: any[]=[];
+
 @Component({
   selector: 'app-app-console',
   templateUrl: './app-console.component.html',
@@ -12,18 +14,27 @@ declare const google: any;
 export class AppConsoleComponent implements OnInit {
 
   location: any;
-  map:any;
+  map: any;
   mapElement: any;
-  marker:any;
-  profile:any;
-  picture:any;
-  name:any;
+  marker: any;
+  profile: any;
+  picture: any;
+  name: any;
 
   constructor(private http: HttpClient, private router: Router) {
   }
 
+  place_marker(locationClick: any): void {
+    this.marker = new google.maps.Marker({
+      position: locationClick,
+      map: this.map
+    });
+  }
+
+
+
   get_location(): void {
-    if(navigator.geolocation) {
+    if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
         (position: GeolocationPosition) => {
           this.location = {
@@ -34,29 +45,38 @@ export class AppConsoleComponent implements OnInit {
             center: this.location,
             zoom: 14,
           });
+
+          this.map.addListener("click", (clickEvent: { latLng: any; }) => {
+            console.log("Mouse Event", clickEvent);
+            this.place_marker(clickEvent.latLng);
+
+            console.log("Latitude & Longitude",clickEvent.latLng.toJSON());
+            arr.push(clickEvent.latLng.toJSON());
+            console.log("Array", arr);
+          });
+
           var myicon = {
             url: this.picture,
-            scaledSize: new google.maps.Size(32,32),
-            origin: new google.maps.Point(0,0),
-            anchor: new google.maps.Point(0,0),
+            scaledSize: new google.maps.Size(32, 32),
+            origin: new google.maps.Point(0, 0),
+            anchor: new google.maps.Point(0, 0),
           };
           console.log(myicon.url);
           var mlabel = "";
           var namel = this.name.split(" ");
           namel.forEach((element: string) => {
-            mlabel=mlabel+element.charAt(0);
+            mlabel = mlabel + element.charAt(0);
           });
           this.marker = new google.maps.Marker({
             position: this.location,
             icon: myicon,
             map: this.map,
-            label: {text: mlabel, color:"white"},
+            label: { text: mlabel, color: "white" },
           });
         }
-        );
-      }
-      else
-    {
+      );
+    }
+    else {
       this.location = {
         lat: 27.2046,
         lng: 77.4977
@@ -69,9 +89,9 @@ export class AppConsoleComponent implements OnInit {
 
   }
 
-  get_profile():void {
+  get_profile(): void {
     var user_name = document.getElementById("username");
-    this.profile = this.http.get<any>(environment.baseUrl+"/user/profile").subscribe(data=>{
+    this.profile = this.http.get<any>(environment.baseUrl + "/user/profile").subscribe(data => {
       user_name!.innerHTML = data.name;
       this.picture = data.picture;
       this.name = data.name;
@@ -85,8 +105,8 @@ export class AppConsoleComponent implements OnInit {
     this.get_profile();
   }
 
-  logout() :void {
-    this.http.get(environment.baseUrl+'/logout');
+  logout(): void {
+    this.http.get(environment.baseUrl + '/logout');
     this.router.navigate(['/home']);
   }
 
--- prider/src/environments/environment.prod.ts
+++ prider/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:800'
+  baseUrl: 'http://localhost:80'
 };
